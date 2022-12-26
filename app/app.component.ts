493b68a7f4694efc5dd35796c4866eebd8d11588
Oldřich Todt <todt.oldrich@outlook.com>
2022-12-26 21:37:13 +0100

    Add initial app.component
    Create app component and register it in app.module file

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
-      Hello!
+      {{ title }}
     </div>
-  `
+  `,
 })
 export class AppComponent {
-
+  title: string = "Ultimate Angular";
 }
