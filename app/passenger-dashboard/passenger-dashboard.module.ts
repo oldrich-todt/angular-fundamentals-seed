a919a3e4fe7b1e96f064dce1ef970ae1eac83cc5
Oldřich Todt <todt.oldrich@outlook.com>
2022-12-29 10:43:52 +0100

    Add passenger dashboard module

---
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [CommonModule],
+})
+export class PassengerDashboardModule {}
